64d332bf046a81b3e8c359fd577b0d22148abd43
Sofiqul Islam <schoolwab@gmail.com>
2020-03-18 00:10:45 +0600

    Add Redirect to Homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect , useLocation } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 
 // import css
@@ -17,20 +17,19 @@ function App() {
   return (
     <Fragment>
       <Header />
-        <Container style={{margin:'75px auto'}}>
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/faq" component={FAQ} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/f" component={Favourite} />
-            <Route path="/v/:id" component={Single} />
-            <Route path="/the100" component={Home} />
-            <Route path="/" exact component={Home} />
-          </Switch>
-        </Container>
+      <Container style={{ margin: "75px auto" }}>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/faq" component={FAQ} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/f" component={Favourite} />
+          <Route path="/v/:id" component={Single} />
+          <Route path="/" exact component={Home} />
+          <Redirect from="/the100" to="/" />
+        </Switch>
+      </Container>
       <Footer />
     </Fragment>
   );
 }
-
 export default App;
